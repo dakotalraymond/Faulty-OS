a957a7dd9b83ff92bf8647e2669f8bb59b312b15
Dakota Raymond <dakota.raymond@sdl.usu.edu>
2019-06-16 21:49:47 -0600

    Faulty OS project "work" added

---
+++ FaultyOS/FaultyOS/Worker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace FaultyOS
+{
+  public class Worker
+  {
+    private int currentState;
+
+    private bool completed = false;
+
+    private Timer timer;
+
+    public Worker()
+    {
+
+    }
+
+    public void StartWork()
+    {
+      using (new SingleGlobalInstance(200000))
+      {
+        while (currentState <= 100)
+        {
+          Thread.Sleep(1000);
+          Console.WriteLine(currentState++);
+        }
+        Console.WriteLine("Work completed, stopping");
+        Console.ReadKey();
+      }
+    }
+  }
+}
